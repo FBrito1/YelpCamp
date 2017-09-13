9e9183329d966161ee13d5ebdb56cc120458747b
Fabio <luan.tecno@gmail.com>
2017-09-13 14:26:53 +0000

    added mongo lab e aruumei o login

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
 
 
-mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+//mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+mongoose.connect("mongodb://fabio:F15089710.@ds135444.mlab.com:35444/yelpcampfabio",{useMongoClient: true});
 mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
