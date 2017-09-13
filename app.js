6d49684991b1282bcfb8ec1008c2eb1af51f356e
Fabio <luan.tecno@gmail.com>
2017-09-13 14:54:12 +0000

    add the dn url

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
 
 
-//mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
-mongoose.connect("mongodb://fabio:F15089710.@ds135444.mlab.com:35444/yelpcampfabio",{useMongoClient: true});
+
+
+mongoose.connect(process.env.DATABASEURL, {useMongoClient: true});
+//mongoose.connect("mongodb://fabio:F15089710.@ds135444.mlab.com:35444/yelpcampfabio",{useMongoClient: true});
 mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
